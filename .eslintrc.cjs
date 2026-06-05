a01d39dc16d09eb012872da1e1923f63500e77d4
cooper <13425143012@163.com>
2026-06-05 15:27:58 +0800

    chore: 配置 ESLint 与 Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: 2022, sourceType: "module" },
+  settings: { react: { version: "18.3" } },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
+  ignorePatterns: ["dist", "node_modules", "*.cjs", "vite.config.ts", "vitest.config.ts"],
+};
